e9a6c6808a67fd0ac187471641bc1210a6124580
tonya <tonya@tokia.dev>
2023-08-07 20:49:21 +0000

    fix: dont check auth

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -15,8 +15,12 @@ export const ourFileRouter = {
       // This code runs on your server before upload
       const user = await auth(req, res);
 
+      console.log('user', user);
+
+      // FIXME: auth is not working, so we're just going to allow all uploads for now
+
       // If you throw, the user will not be able to upload
-      if (user?.user?.name !== 'tonya_') throw new Error('Unauthorized');
+      // if (user?.user?.name !== 'tonya_') throw new Error('Unauthorized');
 
       // This is passed to onUploadComplete as metadata
       return {};
