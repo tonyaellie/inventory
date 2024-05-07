61238accb724b10718610d01610fd1db1e8c67ee
tonyaellie <tonya@tokia.dev>
2024-05-07 18:17:07 +0000

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { type AppType } from 'next/app';
+import Head from 'next/head';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 
 import { api } from '@/utils/api';
+
 import '@/styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -11,6 +13,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <script
+          defer
+          data-domain="inventory.tokia.dev"
+          src="https://ingest.tokia.dev/js/script.js"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
